43377c6ae85f982f6cbe096230c0df6f79b212a4
Frédéric Fort <frederic.fort3@univ-tlse3.fr>
2018-04-09 22:30:14 +0200

    main.py: removed useless imports

--- src/main.py
+++ src/main.py
@@ -1,11 +1,4 @@
-import discord
 from secrets import key
-from utils import *
-import logging
-from botgram import BotGram
-from parseresult import *
-from concurrent.futures import TimeoutError
-from pebble import ProcessPool
 from wintermute import Wintermute
 
 if __name__ =='__main__':
