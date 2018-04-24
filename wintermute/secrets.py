46e9eb1a1af4c981bfba0b018895f64bc7710de1
Frédéric Fort <frederic.fort3@univ-tlse3.fr>
2018-04-24 20:54:44 +0200

    secrets.py: switched to getenv

--- wintermute/secrets.py
+++ wintermute/secrets.py
@@ -1,8 +1,8 @@
-from os import environ
+from os import getenv
 
-key = environ["DISCORD_TOKEN"]
+key = getenv("DISCORD_TOKEN")
 
-mkm_app_token = environ["MKM_APP_TOKEN"]
-mkm_app_secret = environ["MKM_APP_SECRET"]
-mkm_access_token = environ["MKM_ACCESS_TOKEN"]
-mkm_access_token_secret = environ["MKM_ACCESS_TOKEN_SECRET"]
+mkm_app_token = getenv("MKM_APP_TOKEN")
+mkm_app_secret = getenv("MKM_APP_SECRET")
+mkm_access_token = getenv("MKM_ACCESS_TOKEN")
+mkm_access_token_secret = getenv("MKM_ACCESS_TOKEN_SECRET")
